0e49f4d2934445c0f6895949ec6286900c11435e
postrstk <postrstk@gmail.com>
2016-03-09 19:53:34 +0300

    GUI: dev

--- widget.cpp
+++ widget.cpp
@@ -7,18 +7,42 @@ Widget::Widget(QWidget *parent) :
 {
 
     ui->setupUi(this);
-    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(anySlot()));
+    scene = new paintScene(ui->graphicsView_2);
+    tool = new ToolsBar();
+    QString filename = qApp->applicationDirPath()+"/minion_1.jpg";
+    scene->setBrush(tool);
+    scene->addPixmap(QPixmap(qApp->applicationDirPath()+"/2.PNG"));
+    qDebug()<<filename;
+    QGraphicsScene *test = new QGraphicsScene(ui->graphicsView_2);
+    test->addPixmap(QPixmap(filename));
+
+    ui->graphicsView_2->setScene(test);
+   // ui->graphicsView_2->setScene(scene);
+
+    connect(ui->btnColor, SIGNAL(clicked()),
+            this, SLOT(selectColor()));
+    //connect(ui->chckRuber, SIGNAL(clicked(bool)),
+      //      tool, SLOT(selectRuber(bool)));
+    connect(ui->cmbWidth, SIGNAL(currentIndexChanged(int)),
+            tool, SLOT(changeWidth(int)));
+    connect(ui->btnSetBrush, SIGNAL(clicked()),
+            tool, SLOT(setBrush()));
+    connect(ui->btnSetRuber, SIGNAL(clicked()),
+            tool, SLOT(setRuber()));
 }
 
 Widget::~Widget()
 {
     delete ui;
 }
-void Widget::anySlot(){
-    ui->stackedWidget->setCurrentIndex(2);
-}
-
-void Widget::on_pushButton_clicked()
+void Widget::selectColor()
 {
 
+    QColor selectedColor = QColorDialog::getColor();
+    ui->btnColor->setStyleSheet("background-color: rgb("
+                                +QString::number(selectedColor.red())+","
+                                +QString::number(selectedColor.green())+","
+                                +QString::number(selectedColor.blue())+");");
+    tool->changeColor(selectedColor);
 }
+
--- widget.h
+++ widget.h
@@ -2,7 +2,13 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QTimer>
+#include <QResizeEvent>
+#include <QColor>
+#include <QColorDialog>
+#include <QString>
+#include "toolsbar.h"
+#include "paintscene.h"
 namespace Ui {
 class Widget;
 }
@@ -16,12 +22,15 @@ public:
     ~Widget();
 
 private slots:
-    void on_pushButton_clicked();
-
-    void anySlot();
+    void selectColor();
+    //void slotTimer();
 
 private:
     Ui::Widget *ui;
+    ToolsBar *tool;
+    //QTimer *timer;
+    paintScene *scene;
+    //void resizeEvent(QResizeEvent * event);
 };
 
 #endif // WIDGET_H
